369be128fad83796f1d9d5dc06fd88a52fc5b7ab
ghanigh <rhani.94@proton.me>
2024-06-03 16:01:19 +0200

    observer

--- observer.js
+++ observer.js
@@ -10,3 +10,16 @@ class Sujet{
 
     }
 }
+class Observer{
+    update(message){
+        console.log(`L'observateur a reçu le message :${message}`)
+    }
+}
+
+const sujet = new Subjet();
+const observer1 = new Observer();
+const observer2 = new Observer();
+
+sujet.addObserver(observer1);
+sujet.addObserver(observer2);
+sujet.notifierObservers("coucou");
