e2b7c6d703a1bdf00d4a1a44497f8d516329040d
ghanigh <rhani.93@proton.me>
2024-06-04 12:08:54 +0200

    Polymorphisme

--- Factory/exercice.js
+++ Factory/exercice.js
@@ -1,36 +1,85 @@
 // SANS DESIGN PATTERN
 class Audi {
     Vitesse (){}
-    COlor(){}
+    Color(){}
 }
 
 class BMW {
     Vitesse (){}
-    COlor(){}
+    Color(){}
 }
 
 class Mercedes {
     Vitesse (){}
-    COlor(){}
+    Color(){}
 }
 
 const objet1 = new Audi
 const objet2 = new BMW
 const objet3 = new Mercedes
 
-// AVEC DESIGN PATTERN
-class Audi {
-    Vitesse (){}
-    COlor(){}
+// AVEC DESIGN PATTERN FACTORY
+class Voiture{
+    vitess() {
+        throw new Error("Cette méthode doit être implémentée.")
+    }
 }
 
-class BMW {
-    Vitesse (){}
-    COlor(){}
+class Color{
+    vitess() {
+        throw new Error("Cette méthode doit être implémentée.")
+    }
 }
 
-class Mercedes {
+
+
+class Audi extends Voiture{
     Vitesse (){}
-    COlor(){}
+    Color(){}
 }
 
+class BMW extends Voiture{
+    Vitesse (){}
+    Color(){}
+}
+
+class Mercedes extends Voiture{
+    Vitesse (){}
+    Color(){}
+}
+
+// classe CartFactory pour créer des instances de voiture
+class CartFactory {
+    create(){
+        switch(marque){
+            case 'Audi':
+            return new Audi();
+            case 'BMW':
+            return new BMW();
+            case 'Mercedes':
+            return new Mercedes();
+            default:
+                throw new Error("TYpe de voiture inconnu");
+        }
+    }
+
+}
+
+// Utilisation de la fabrique sans méthode statique
+const factory = new CartFactory;
+const objetBMW = factory.create("BMW")
+const objetAudi = factory.create("Audi")
+const objetMercedes = factory.create("Mercedes")
+
+/* Voiture = new Voiture()
+Voiture1 = new Voiture()
+Voiture2 = new Voiture()
+
+voiture.color = 'red'
+voiture1.color = 'bleu'
+voiture2.color = 'green'
+
+Voiture.color('pink') */
+
+// Utilisation de la fabrique avec méthode statique
+const objetAudi2 = CartFactory.createStatic('Audi')
---
+++ Polymorphise/Polymorphise.js
@@ -0,0 +1,5 @@
+/* 
+Le Polymorphisme vous permet de traiter différent objet de manière uniforme en
+utilisant une interface commune.
+
+*/
